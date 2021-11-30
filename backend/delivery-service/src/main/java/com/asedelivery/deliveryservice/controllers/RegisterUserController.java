294450b767e0597c7166e37808d3181cd12f8b43
Pellumb Baboci <pellumb.baboci@tum.de>
2021-11-30 01:29:42 +0100

    added delivery service with functionality register user but not yet in sync with userDatabase

---
+++ backend/delivery-service/src/main/java/com/asedelivery/deliveryservice/controllers/RegisterUserController.java
@@ -0,0 +1,96 @@
+package com.asedelivery.deliveryservice.controllers;
+
+import com.asedelivery.deliveryservice.models.ERole;
+import com.asedelivery.deliveryservice.models.Role;
+import com.asedelivery.deliveryservice.models.User;
+import com.asedelivery.deliveryservice.payload.request.RegisterUserRequest;
+import com.asedelivery.deliveryservice.payload.response.MessageResponse;
+import com.asedelivery.deliveryservice.repository.RoleRepository;
+import com.asedelivery.deliveryservice.repository.UserRepository;
+import com.asedelivery.deliveryservice.security.jwt.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashSet;
+import java.util.Set;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/user/auth")
+public class RegisterUserController {
+	@Autowired
+	AuthenticationManager authenticationManager;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	RoleRepository roleRepository;
+
+	@Autowired
+	PasswordEncoder encoder;
+
+	@Autowired
+	JwtUtils jwtUtils;
+
+	@PostMapping("/register")
+	public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterUserRequest registerUserRequest) {
+		System.out.println(registerUserRequest.getRoles());
+		if (userRepository.existsByUsername(registerUserRequest.getUsername())) {
+			return ResponseEntity
+					.badRequest()
+					.body(new MessageResponse("Error: Username is already taken!"));
+		}
+
+		if (userRepository.existsByEmail(registerUserRequest.getEmail())) {
+			return ResponseEntity
+					.badRequest()
+					.body(new MessageResponse("Error: Email is already in use!"));
+		}
+
+		// Create new user's account
+		User user = new User(registerUserRequest.getUsername(),
+				registerUserRequest.getEmail(),
+							 encoder.encode(registerUserRequest.getPassword()));
+
+		Set<String> strRoles = registerUserRequest.getRoles();
+		System.out.println(strRoles);
+		Set<Role> roles = new HashSet<>();
+
+		if (strRoles == null) {
+			Role userRole = roleRepository.findByName(ERole.ROLE_CUSTOMER)
+					.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+			roles.add(userRole);
+		} else {
+			strRoles.forEach(role -> {
+				switch (role) {
+				case "dispatcher":
+					Role adminRole = roleRepository.findByName(ERole.ROLE_DISPATCHER)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(adminRole);
+
+					break;
+				case "deliverer":
+					Role modRole = roleRepository.findByName(ERole.ROLE_DELIVERER)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(modRole);
+
+					break;
+				default:
+					Role userRole = roleRepository.findByName(ERole.ROLE_CUSTOMER)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(userRole);
+				}
+			});
+		}
+
+		user.setRoles(roles);
+		userRepository.save(user);
+
+		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+	}
+}
