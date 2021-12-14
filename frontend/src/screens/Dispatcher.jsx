ad2ab95c327aaecb89905700012950d1eef1cd9d
Eugenia Clarissa Anjani <e.clarissa.anjani@tum.de>
2021-12-14 16:21:24 +0100

    Updated some links and updated the dispatcher view to link to the create new user. Edit User page is still being worked on.

--- frontend/src/screens/Dispatcher.jsx
+++ frontend/src/screens/Dispatcher.jsx
@@ -1,27 +1,127 @@
 import React, { useEffect, useState } from 'react'
 import DispatcherService from '../services/dispatcher.service';
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import { makeStyles } from '@mui/styles'
+import { Container, Grid } from '@mui/material'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBrain, faChartLine, faUserLock } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
+
+const useStyles = makeStyles((theme) => ({
+  boxStyle: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+  },
+  cardStyle: {
+    height: '30vh',
+    padding: 10,
+  },
+}))
+
+const cardPointOne = (
+  <React.Fragment>
+    <CardContent>
+      <FontAwesomeIcon icon={faBrain} />
+      <Typography sx={{ fontSize: 20 }} color='text.secondary' gutterBottom>
+        User Management
+      </Typography>
+    </CardContent>
+    <CardActions>
+       <Link to='/edituser' color='inherit' className="button"><Button size='small' variant='contained'>Edit users</Button></Link>
+    </CardActions>
+    <CardActions>
+       <Link to='/createnewuser' color='inherit' className="button"><Button size='small' variant='contained'>Create new users</Button></Link>
+    </CardActions>
+  </React.Fragment>
+)
+
+const cardPointTwo = (
+  <React.Fragment>
+    <CardContent>
+      <FontAwesomeIcon icon={faChartLine} />
+
+      <Typography sx={{ fontSize: 20 }} color='text.secondary' gutterBottom>
+        Delivery Management
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button variant='contained' size='small'>Edit deliveries</Button>
+    </CardActions>
+    <CardActions>
+       <Button size='small' variant='contained'>Create new deliveries</Button>
+    </CardActions>
+  </React.Fragment>
+)
+
+const cardPointThree = (
+  <React.Fragment>
+    <CardContent>
+      <FontAwesomeIcon icon={faUserLock} />
+
+      <Typography sx={{ fontSize: 20 }} color='text.secondary' gutterBottom>
+        Box management
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size='small' variant='contained'>Edit boxes</Button>
+    </CardActions>
+    <CardActions>
+       <Button size='small' variant='contained'>Create newboxes</Button>
+    </CardActions>
+  </React.Fragment>
+)
 
 const Dispatcher = () => {
+    const classes = useStyles()
+      return (
+        <Box px={{ xs: 3, sm: 10 }} py={{ xs: 5, sm: 10 }} bgcolor='text.secondary' color='white' className={classes.boxStyle}>
+          <Container maxWidth='lg'>
+            <Grid container spacing={5}>
+              <Grid item xs={12} sm={4}>
+                <Card variant='outlined' className={classes.cardStyle}>
+                  {cardPointOne}
+                </Card>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Card variant='outlined' className={classes.cardStyle}>
+                  {cardPointTwo}
+                </Card>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Card variant='outlined' className={classes.cardStyle}>
+                  {cardPointThree}
+                </Card>
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
+      )
+    //const [content, setContent] = useState("");
 
-    const [content, setContent] = useState("");
 
-
-  useEffect(() => {
+  //useEffect(() => {
       // here you get the content from backend needed for dispatcher
-    DispatcherService.getDispatcherView().then(
+    /*DispatcherService.getDispatcherView().then(
       (response) => {
         setContent(response.data);
       }
     ).catch(error => {
         setContent(error.response.data.message);
     });
-  }, []);
-    return (
-        <div>
-            <p>dispatcher</p>
-            <h1>{content}</h1>
-        </div>
-    )
+  }, []); */
+    //return (
+      //  <div>
+        //    <p>dispatcherx</p>
+      //      <h1>{content}</h1>
+       // </div>
+   // )
 }
 
 export default Dispatcher
