10998e27bba7f980eb3666deb666745f86d2fcd7
Pellumb Baboci <pellumbbaboci@gmail.com>
2022-01-26 01:33:38 +0100

    added delivery mongo model

---
+++ backend/delivery-service/src/main/java/com/asedelivery/deliveryservice/repository/DeliveryRepository.java
@@ -0,0 +1,7 @@
+package com.asedelivery.deliveryservice.repository;
+
+import com.asedelivery.deliveryservice.models.Delivery;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface DeliveryRepository extends MongoRepository<Delivery, String> {
+}
