1f3f4c5872204b0db9ab01eca90132c8a29270f5
Eugenia Clarissa Anjani <e.clarissa.anjani@tum.de>
2022-02-05 21:18:07 +0100

    updated delete box and user, and updated views of box and user management to include modals. Updated views to edit the user+box and reload the pages

--- frontend/src/screens/ListBoxes.jsx
+++ frontend/src/screens/ListBoxes.jsx
@@ -1,137 +1,232 @@
-import { makeStyles } from '@mui/styles'
-import { Paper, Button, TextField, RadioGroup, FormLabel, FormControl, FormControlLabel, Radio} from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import Box from '@mui/material/Box'
-import Card from '@mui/material/Card'
-import CardActions from '@mui/material/CardActions'
-import CardContent from '@mui/material/CardContent'
-import Typography from '@mui/material/Typography'
-import { Container, Grid } from '@mui/material'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBrain, faChartLine, faUserLock } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom'
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import ProjectTable from "../components/ProjectTable";
-import DispatcherService from '../services/dispatcher.service'
-import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
+import {makeStyles} from '@mui/styles';
+import {Button, Paper, Stack,} from '@mui/material';
+import React, {useEffect, useState} from 'react';
+import DispatcherService from "../services/dispatcher.service";
+import EditBoxModal from "../components/Boxes/EditBoxModal";
+import DeleteBoxModal from "../components/Boxes/DeleteBoxModal";
+import NewBoxModal from "../components/Boxes/NewBoxModal";
+import {DataGrid, GridToolbar} from "@mui/x-data-grid";
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    position: 'relative',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'left',
-    alignItems: 'center',
-  },
-  boxManagementRoot: {
-    margin: 'auto',
-    paddingTop: 10,
-  },
-  boxManagementPaper: {
-    width: '1000px',
-    padding: theme.spacing(4),
-  },
+    container: {
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'left',
+        alignItems: 'center',
+    },
+    boxManagementRoot: {
+        margin: 'auto',
+        height: '60vh',
+        minHeight: '50vh',
+        paddingTop: 10,
+    },
+    boxManagementPaper: {
+        width: '1000px',
+        padding: theme.spacing(4),
+    },
 
-  boxManagementRow: {
-    paddingTop: theme.spacing(1),
-    paddingBottom: theme.spacing(1),
-    '&:last-child': {
-      paddingBottom: theme.spacing(0),
+    boxManagementRow: {
+        paddingTop: theme.spacing(1),
+        paddingBottom: theme.spacing(1),
+        '&:last-child': {
+            paddingBottom: theme.spacing(0),
+        },
+        '&:first-child': {
+            paddingTop: theme.spacing(0),
+        },
     },
-    '&:first-child': {
-      paddingTop: theme.spacing(0),
+    submitButtons: {
+        marginTop: 20,
+        display: 'flex',
+        justifyContent: 'space-between',
+        color: 'secondary',
+        alignItems: 'center',
+        width: 1,
     },
-  },
-  submitButtons: {
-    marginTop: 20,
-    display: 'flex',
-    justifyContent: 'space-between',
-    color: 'secondary',
-    alignItems: 'center',
-    width: 1,
-  },
-  submitButton: {
-    width: 1,
-    textAlign: 'center',
-    color: 'secondary',
-  },
+    submitButton: {
+        width: 1,
+        textAlign: 'center',
+        color: 'secondary',
+    },
 }))
 
+const columns = [
+    { field: 'id', headerName: 'Box ID', width: 200, editable: 'false'  },
+    { field: 'name', headerName: 'Box Name', width: 200, editable:'true'  },
+    { field: 'address', headerName: 'Box Address', width: 200, editable:'true'  },
+    { field: 'status', headerName: 'Box Status', width: 200, editable:'true'  },
+];
 
 
-function TableItem(data) {
-    var newrows = [];
-    data.map( (item) => {
-            var itemInfo = { "id": item.id,
-                "name": item.name,
-                "status": item.status,
-                "address": item.address,
-                };
-            newrows.push(itemInfo)});
-    return newrows;
+//    console.log(dispatcherId);
+function ListDeliveries(){
+    const classes = useStyles();
+    const title = "List of your Boxes";
+    const description = "Manage your boxes";
+
+    const [BoxData, setBoxData] = useState([])
+    function readBoxes(data) {
+        let newRows = [];
+        let name = "";
+        let address = "";
+        let status =  "";
+        let id = "";
+
+        try {
+            newRows = data.map( (item) => {
+                id = item.id;
+                name = item.name;
+                status =  item.status;
+                address = item.address
+                return {
+                    "id": item.id,
+                    "name": item.name,
+                    "address": item.address,
+                    "status": item.status,
+                }
+                //newRows.push(itemInfo)
+            });
+        }
+        catch (e) {
+            console.error(e);
+        }
+
+        return newRows;
+    }
+
+    try {
+        useEffect(()=>{
+            DispatcherService.getBoxes()
+                .then(function (response) {
+                    console.log(response);
+                    setBoxData(readBoxes(response.data));
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+        }, [])
+        console.log(BoxData)
+        //rows = readDeliveries(UserData);
+    }
+    catch (e) {
+        console.error(e);
+    }
+    const [createModalIsOpen, setCreateModalIsOpen] = useState(false);
+    const [deleteModalIsOpen, setDeleteModalIsOpen] = useState(false);
+    const [changeModalIsOpen, setChangeModalIsOpen] = useState(false);
+
+    function openCreateModalHandler() {
+        // switch to the state where the modal is open
+        setCreateModalIsOpen(true);
+    }
+    function closeCreateModalHandler() {
+        setCreateModalIsOpen(false)
+    }
+
+    function openDeleteModalHandler() {
+        // switch to the state where the modal is open
+        setDeleteModalIsOpen(true);
+    }
+    function closeDeleteModalHandler() {
+        setDeleteModalIsOpen(false)
+    }
+
+    function openChangeModalHandler() {
+        // switch to the state where the modal is open
+        setChangeModalIsOpen(true);
+    }
+    function closeChangeModalHandler() {
+        setChangeModalIsOpen(false)
+    }
+
+    //const [rows, setRows] = useState(readDeliveries(UserData));
+    const [selectedIds, setSelectedIds] = useState([]);
+    const [deletedRows, setDeletedRows] = useState([]);
+    const [purgeMode, setPurgeMode] = useState(true);
+    const [name, setName] = useState('')
+    const [address, setAddress] = useState('')
+    const [status, setStatus] = useState('')
+    const [setSubmitted] = useState(false)
+    const handleSelectionChange = (selection) => {
+        //setSelectedRows(selection.rows);
+        //console.log(selectedRows)
+        setSelectedIds(selection);
+        //const selectedRowData = rows.filter((row) =>
+        console.log(selection);
+    };
+    console.log(selectedIds);
+
+    const handleSave = (e) => {
+         //changing input once already entered
+           const handleName = (e) => {
+             setName(e.target.value)
+             setSubmitted(false)
+           }
+
+           const handleAddress = (e) => {
+             setAddress(e.target.value)
+             setSubmitted(false)
+           }
+
+           const handleStatus = (e) => {
+             setStatus(e.target.value)
+             setSubmitted(false)
+           }
+         console.log("entered the save")
+         console.log(selectedIds);
+         for (const element of selectedIds) {
+         DispatcherService.postBox(selectedIds)
+             .then(function (selectedIds) {
+                 console.log(selectedIds);
+             })
+             .catch((error) => {
+             console.log(error.response)
+             })
+           }
+         }
+
+    return (<div className={classes.container}>
+                <h1> {title} </h1>
+                <h3> {description} </h3>
+                    <Stack spacing={2} direction="row">
+                        <Button onClick={openCreateModalHandler} color='secondary' variant='contained' edge='end' aria-label='account of current user' aria-controls={'login-menu'} aria-haspopup='true'>
+                            Create
+                        </Button>
+                        <NewBoxModal handleOpen={openCreateModalHandler} handleClose={closeCreateModalHandler} open={createModalIsOpen}/>
+                        <Button onClick={openDeleteModalHandler} color='secondary' variant='contained' edge='end' aria-label='account of current user' aria-controls={'login-menu'} aria-haspopup='true'>
+                            Delete
+                        </Button>
+                        <DeleteBoxModal selectedRows={selectedIds} handleOpen={openDeleteModalHandler} handleClose={closeDeleteModalHandler} open={deleteModalIsOpen}/>
+                        <Button onClick={openChangeModalHandler} color='secondary' variant='contained' edge='end' aria-label='account of current user' aria-controls={'login-menu'} aria-haspopup='true'>
+                            Edit Boxes
+                        </Button>
+                        <EditBoxModal selectedRows={selectedIds} handleOpen={openChangeModalHandler} handleClose={closeChangeModalHandler} open={changeModalIsOpen}/>
+                        <Button onClick={handleSave} color='secondary' variant='contained' edge='end' aria-label='account of current user' aria-controls={'login-menu'} aria-haspopup='true'>
+                            Save
+                        </Button>
+                    </Stack>
+                <Paper className={classes.boxManagementPaper} component='form'>
+                    <div className={classes.container}>
+                        <div style={{ height: 800, width: '100%' }}>
+                            <DataGrid
+                                rows={BoxData}
+                                columns={columns}
+                                pageSize={15}
+                                rowsPerPageOptions={[15]}
+                                checkboxSelection
+                                onSelectionModelChange={handleSelectionChange}
+                                editMode="row"
+                                HorizontalAlign="Center"
+                                components={{
+                                    Toolbar: GridToolbar,
+                                }}
+                            />
+                        </div>
+                    </div>
+                </Paper>
+            </div>);
 }
 
-
-const ListBoxes = () => {
-    const classes = useStyles()
-
-    const title = "List of your Boxes";
-    const description = "See assigned boxes and change the status of assigned deliveries";
-
-    const [loadingData, setLoadingData] = useState(true);
-    const [BoxData, setBoxData] = useState([])
-
-    const columns = [
-        {title: "Box ID", field : "id", headerName: "Box ID", width:150},
-        {title: "Box Status", field : "status", headerName: "Box Status", width:150},
-        {title: "Box Address", field : "address", headerName: "Box Address", width:150},
-        {title: "Box Name", field : "name", headerName: "Box Name", width:150},
-    ]
-
-     // get the users data
-       useEffect(()=>{
-         DispatcherService.getBoxes()
-         .then((data) => {
-            setBoxData(data.data)
-         })
-         .catch((error) => {
-            console.log(error)
-         })
-       }, [])
-       console.log(BoxData)
-
-    return (
-      <div className={classes.container}>
-        <h1> {title} </h1>
-        <h3> {description} </h3>
-        <div className={classes.boxManagementRoot}>
-                    <ButtonToolbar>
-                      <Link to='/createnewboxes' color='inherit' className='button'>
-                        <Button color='secondary' variant='contained' edge='end' aria-label='account of current user' aria-controls={'login-menu'} aria-haspopup='true'>
-                            Create
-                         </Button>{' '}
-                      </Link>
-                      <Button color='secondary' variant='contained' edge='end' aria-label='account of current user' aria-controls={'login-menu'} aria-haspopup='true'
-                       type='submit'>
-                         Edit
-                      </Button>{' '}
-                      <Button color='secondary' variant='contained' edge='end' aria-label='account of current user' aria-controls={'login-menu'} aria-haspopup='true'
-                         type='submit'>
-                         Delete
-                     </Button>{' '}
-                     </ButtonToolbar>
-                   <Paper className={classes.boxManagementPaper} component='form'>
-                      <ProjectTable rows={TableItem(BoxData)} title={title} description={description} columns={columns}/>
-                  </Paper>
-                </div>
-              </div>
-            )
-          }
-
-export default ListBoxes
-
+export default ListDeliveries
+//selectedIDs.has(row.id.toString));
