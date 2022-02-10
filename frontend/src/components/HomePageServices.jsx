47e437cd939f7040da027216a5fa4a11e2a206cf
Eugenia Clarissa Anjani <e.clarissa.anjani@tum.de>
2022-02-10 23:38:43 +0100

    Home page update, added contributions file

--- frontend/src/components/HomePageServices.jsx
+++ frontend/src/components/HomePageServices.jsx
@@ -3,12 +3,16 @@ import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import { makeStyles } from '@mui/styles'
 import { Container, Grid } from '@mui/material'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBrain, faChartLine, faUserLock } from '@fortawesome/free-solid-svg-icons'
+import Smart from '../images/trackdelivery.jpg'
+import Fast from '../images/fast.jpg'
+import Reliable from '../images/reliable.jpg'
 
 const useStyles = makeStyles((theme) => ({
   boxStyle: {
@@ -18,58 +22,65 @@ const useStyles = makeStyles((theme) => ({
     height: '100vh',
   },
   cardStyle: {
-    height: '30vh',
+    minHeight: '20vh',
     padding: 10,
   },
 }))
 
 const cardPointOne = (
   <React.Fragment>
+     <CardMedia
+       component="img"
+       height="200"
+       width= "auto"
+       src={Smart}
+     />
     <CardContent>
-      <FontAwesomeIcon icon={faBrain} />
-      <Typography sx={{ fontSize: 20 }} color='text.secondary' gutterBottom>
+      <FontAwesomeIcon icon={faBrain} fontSize={40}/>
+      <Typography variant="h5" gutterBottom>
         Smart
       </Typography>
 
-      <Typography variant='body2'>You can track any order online in real time and check the status</Typography>
+      <Typography variant='h6'>Track any order online in real-time and check the status</Typography>
     </CardContent>
-    <CardActions>
-      <Button size='small'>Learn More</Button>
-    </CardActions>
   </React.Fragment>
 )
 
 const cardPointTwo = (
   <React.Fragment>
+     <CardMedia
+       component="img"
+       height="200"
+       width= "auto"
+       src={Fast}
+     />
     <CardContent>
-      <FontAwesomeIcon icon={faChartLine} />
-
-      <Typography sx={{ fontSize: 20 }} color='text.secondary' gutterBottom>
+      <FontAwesomeIcon icon={faChartLine} fontSize={40}/>
+      <Typography variant="h5" gutterBottom>
         Fast
       </Typography>
 
-      <Typography variant='body2'>Pick up your deliveries without having to wait in line.</Typography>
+      <Typography variant='h6'>Pick up your deliveries without having to wait in line</Typography>
     </CardContent>
-    <CardActions>
-      <Button size='small'>Learn More</Button>
-    </CardActions>
   </React.Fragment>
 )
 
 const cardPointThree = (
   <React.Fragment>
+     <CardMedia
+       component="img"
+       height="200"
+       width= "auto"
+       src={Reliable}
+     />
     <CardContent>
-      <FontAwesomeIcon icon={faUserLock} />
-
-      <Typography sx={{ fontSize: 20 }} color='text.secondary' gutterBottom>
+      <FontAwesomeIcon icon={faUserLock} fontSize={40}/>
+      <Typography variant="h5" gutterBottom>
         Reliable
       </Typography>
 
-      <Typography variant='body2'>ASE Delivery provides secure collection and secure authorization</Typography>
+      <Typography variant='h6'>Benefit from secure collection and authorization</Typography>
     </CardContent>
-    <CardActions>
-      <Button size='small'>Learn More</Button>
-    </CardActions>
   </React.Fragment>
 )
 
@@ -78,6 +89,9 @@ export default function HomePageServices() {
   return (
     <Box px={{ xs: 3, sm: 10 }} py={{ xs: 5, sm: 10 }} bgcolor='text.secondary' color='white' className={classes.boxStyle}>
       <Container maxWidth='lg'>
+        <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+          <h1>ASE Delivery System</h1>
+        </div>
         <Grid container spacing={5}>
           <Grid item xs={12} sm={4}>
             <Card variant='outlined' className={classes.cardStyle}>
